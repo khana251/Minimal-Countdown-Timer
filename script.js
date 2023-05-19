d03bfcea3f61239758dda6794c8a4785bd7b94c9
Amaan Ahmad Khan <59571647+khana251@users.noreply.github.com>
2023-05-18 22:06:25 -0400

    Added all files to Repo
    Will add readme soon...

---
+++ script.js
@@ -0,0 +1,36 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2024';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    
+    const diffTime = Math.abs(newYearsDate - currentDate);
+
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+    const diffHours = Math.floor(diffTime / (1000 * 60 * 60)) % 24;
+    const diffMinutes = Math.floor(diffTime / (1000 * 60)) % 60;
+    const diffSeconds = Math.floor(diffTime / (1000)) % 60;
+    console.log(diffDays);
+    console.log(diffHours % 24);
+    console.log(diffMinutes % 60);
+    console.log(diffSeconds % 60);
+    
+    daysEl.innerHTML = formatTime(diffDays);
+    hoursEl.innerHTML = formatTime(diffHours);
+    minutesEl.innerHTML = formatTime(diffMinutes);
+    secondsEl.innerHTML = formatTime(diffSeconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
